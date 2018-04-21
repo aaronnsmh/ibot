96147fce26d3142877aa553527f2c467e2a2cc7d
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 18:38:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
     let nameid = client.user.id;
-    client.user.setActivity('over Discord | -help', { type: 'WATCHING' });
     client.user.setPresence({ activity: { name: 'at Koala Cafe!' }, status: 'online' })
   .then(console.log)
   .catch(console.error);
