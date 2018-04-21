6560af4875be606f905e80e6fac3d64313e181ca
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:34:15 +0100

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,6 +1,8 @@
 exports.run = (Discord, client, message, args) => {
     
     let name = client.user.username;
+    let thumbnail = client.user.avatarURL;
+
     
 function doMagic8BallVoodoo() {
     var rand = [':fist: Rock', ' :raised_hand: Paper', ':v: Scissors'];
@@ -11,7 +13,7 @@ function doMagic8BallVoodoo() {
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setThumbnail("https://cdn.vectorstock.com/i/1000x1000/91/89/rock-paper-scissors-game-vector-3959189.jpg")
-           	   .setAuthor(name)
+           	   .setAuthor(name,thumbnail)
 		   .setTitle(":fist: Rock, :raised_hand: Paper, :v: Scissors")
 		   .addField("Result: ",doMagic8BallVoodoo())
 		   .setFooter("Requested by " + author + " at ")
