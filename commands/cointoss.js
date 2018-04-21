83e15f6b7252a6095ea2c67c7cd1d95b22b819ca
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:40:10 +0100

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -1,5 +1,6 @@
 exports.run = (Discord, client, message, args) => {
-
+let thumbnail = client.user.avatarURL;
+let name = client.user.username;
 function coinToss() {
     var rand = ['You flipped the coin, it lands on tails.', 'I flipped the coin, it lands on tails.', 'You flipped the coin, it lands on heads.', 'I flipped the coin, it lands on heads.'];
     return rand[Math.floor(Math.random()*rand.length)];
@@ -8,7 +9,7 @@ const author = message.author;
 		   const embed = new Discord.RichEmbed()
 		   .setColor("#00ff00")
 		   .setThumbnail("https://vignette.wikia.nocookie.net/mariokart/images/d/dc/Coin_-_Mario_Kart_Wii.png/revision/latest?cb=20170719161622")
-           .setAuthor(name,thumbnail)
+           	   .setAuthor(name,thumbnail)
 		   .setTitle("Coin Toss")
 		   .addField("Result: ",coinToss())
 		   .setFooter("Requested by " + author + " at ")
