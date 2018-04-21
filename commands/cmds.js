08b5bb15bb422a7e6bbc67fbf9faad474a4f0db4
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:44:38 +0100

    Create cmds.js

---
+++ commands/cmds.js
@@ -0,0 +1,23 @@
+var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+let name = client.user.username;
+let thumbnail = client.user.avatarURL;
+exports.run = (Discord, client, message, args) => {
+
+    message.reply("Commands:");
+    var embedcmds= new Discord.RichEmbed()
+        .setFooter("©" + name + "  | Made by Aaron#1742")
+        .setAuthor(name,thumbnail)
+        .setColor(randomColor)
+        .setTitle(":book:" + name + " Commands")
+        .setDescription("Wondering what I do? Well you sir/ma'am have a very good question! Read on to see why I am here!")
+        .addField(":8ball: 8Ball [QUESTION]", "Responds with what will happen.")
+        .addField(":o: cointoss", "Tosses a coin.")
+        .addField(":frame_photo: avatar [@USER]", "Shows a picture of a tagged persons avatar!")
+        .addField(":loudspeaker: say [STRING]", "The bot says what you ask it to say, well duh.")
+        .addField(":fire: kick/ban [@USER] [REASON]", "The bot kicks/bans the user with the reason you put. Tut tut!")
+        .addField(":exclamation: announce [TITLE] [DESCRIPTION]", "Announces whatever you put in the channel you write it in.")
+        .addField(":clock1: More CMDS coming soon", "What do you expect me to put here?")
+    message.channel.sendEmbed(embedcmds);
+    message.delete(10000)
+
+}
