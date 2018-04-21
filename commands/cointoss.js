8e21cc40f6477e784f8ca09a3299d0cbe58dd00c
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:38:32 +0100

    Update cointoss.js

--- commands/cointoss.js
+++ commands/cointoss.js
@@ -4,5 +4,16 @@ function coinToss() {
     var rand = ['You flipped the coin, it lands on tails.', 'I flipped the coin, it lands on tails.', 'You flipped the coin, it lands on heads.', 'I flipped the coin, it lands on heads.'];
     return rand[Math.floor(Math.random()*rand.length)];
 }
-message.channel.sendMessage(coinToss())
+const author = message.author;
+		   const embed = new Discord.RichEmbed()
+		   .setColor("#00ff00")
+		   .setThumbnail("https://vignette.wikia.nocookie.net/mariokart/images/d/dc/Coin_-_Mario_Kart_Wii.png/revision/latest?cb=20170719161622")
+           .setAuthor(name,thumbnail)
+		   .setTitle("Coin Toss")
+		   .addField("Result: ",coinToss())
+		   .setFooter("Requested by " + author + " at ")
+		   .setTimestamp()
+		   message.channel.send("Cointoss from: " + author);
+		   message.channel.send({embed})
     
+}
