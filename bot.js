e3e432619d5de8d3c1915ae58787bcfad2729ae2
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 17:45:23 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ client.on('message', message => {
 
 
 client.on('message', message => {    
-    if (message.mentions.members.firstKey() === 'nameid') {
+    if (message.mentions.members.firstKey() === '383658506060038144') {
     message.channel.send(':zzz: :zzz: :sleeping: You woke me. How rude! :angry:, if you need me run -cmds to check out the commands!')
    }
     
