cbe9c933476de8bd45f2994c576a4240676b8c9e
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 08:59:08 +0100

    Create ba.js

---
+++ commands/ba.js
@@ -0,0 +1,18 @@
+
+exports.run = (Discord, client, message, args) => {
+
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if (!kUser) return message.channel.send("Can't find user!");
+    let kReason = args.join(" ").slice(22);
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+    if (kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be banned!");
+
+
+    message.guild.member(kUser).ban(kReason);
+    let kickChannel = '419094345858154496';
+    let pkickchannel = message.guild.channels.find(`name`, "logs");
+    message.channel.send('User banned!')
+    if (!pkickChannel) return message.channel.send("I can not find the log channel, the user has been kicked. For logs, get the sever admin to make a channel called `logs`.");
+
+    
+}
