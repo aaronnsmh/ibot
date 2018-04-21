ae194dc3a4df485984e32f13e92750f80a107a1a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:31:44 +0100

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -1,11 +1,24 @@
 exports.run = (Discord, client, message, args) => {
-
+    
+    let name = client.user.username;
+    
 function doMagic8BallVoodoo() {
     var rand = [':fist: Rock', ' :raised_hand: Paper', ':v: Scissors'];
 
     return rand[Math.floor(Math.random()*rand.length)];
 }
-
+    const author = message.author;
+		   const embed = new Discord.RichEmbed()
+		   .setColor("#00ff00")
+		   .setThumbnail("https://cdn.vectorstock.com/i/1000x1000/91/89/rock-paper-scissors-game-vector-3959189.jpg")
+           .setAuthor(name)
+		   .setTitle("Coin Toss")
+		   .addField("Result: ",doMagic8BallVoodoo())
+		   .setFooter("Requested by " + author + " at ")
+		   .setTimestamp()
+		   message.channel.send(":8ball: from: " + author);
+		   message.channel.send({embed})
+    
     message.channel.sendMessage(doMagic8BallVoodoo())
     
 }
