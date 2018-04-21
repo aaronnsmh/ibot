a4983f0cde5ae6ef1e6566944f9145c808e3b150
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-21 13:19:44 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,9 +4,19 @@ exports.run = (Discord, client, message, args) => {
 if(message.mentions.users.first()) { //Check if the message has a mention in it.
           let user = message.mentions.users.first(); //Since message.mentions.users returns a collection; we must use the first() method to get the first in the collection.
           let output = "Avatar URL: " + user.avatarURL;
+          let avatarurl = user.avatarURL;
+          let thumbnail = client.user.avatarURL;
+          let author = message.author;
     
           message.channel.sendMessage(output); //We send the output in the current channel.
-        
+          const embed = new Discord.RichEmbed()
+		   .setColor("#00ff00")
+		   .setThumbnail(thumbnail)
+		   .setTitle(user)
+		   .setImage(avatarurl)
+		   .setFooter("Requested by " + author + " at ")
+		   .setTimestamp()
+		   message.channel.sendEmbed(embed);
           
     } else {
           message.reply("Please tag a user!"); //Reply with a mention saying "Invalid user."
