d8ba1b3cdd95d826a9f66f889d5fcb7858b441b5
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-23 17:01:45 +0100

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -2,7 +2,9 @@ exports.run = (Discord, client, message, args) => {
         let reason = args.slice(1).join(" ");
         let user = message.mentions.users.first();
         let staffc = message.guild.channels.find("name", "logs")
-
+        
+        if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("No can do pal!");
+        
         if (!staffc) 
             return message.reply('I cannot find a mod-log channel');
 
