9443acd6dbdfbaa3a021e16fa75aa75dac471eb2
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-04-24 13:09:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,37 +8,44 @@ var randomColor = Math.floor(Math.random() * 16777215).toString(16);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}! There are no apparent major bugs.`);
-    let nameid = client.user.id;
-    client.user.setPresence({ activity: { name: 'at Koala Cafe!' }, status: 'online' })
-  .then(console.log)
-  .catch(console.error);
-});
-
-client.on('ready', () => {
-    console.log('Koala Bot is ONLINE. No bugs found at this time.');
-
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+    client.user.setActivity(' nothing | =-help', { type: 'WATCHING' });
+    client.user.setStatus("online");
 });
 
 
-
-
-    
-    
+client.on('message', message => { 
     let sender = message.author;
-    
-    if (sender.bot) return;
-    if (message.channel.type === 'dm') {
-        message.channel.send("**Unfortunately we can only read things in a guild (server). You can invite me here: https://discordapp.com/oauth2/authorize?client_id=383658506060038144&scope=bot&permissions=8200**")
+}
+          
+if (sender.bot) return;
+
+     if (message.channel.type === 'dm') {
+        var embederrordm = new Discord.RichEmbed()
+        .setTitle('Error 404:')
+        .setDescription('Our system can only detect things in a guild!')
+        .setImage('https://media.giphy.com/media/gU25raLP4pUu4/giphy.gif')
+        .setFooter('Error 404: DM Message')
+        .setColor('#000000');
+        message.channel.send(embederrordm);
         return;
     }
     
-    
+client.on("message", message => {
+  const prefixMention = new RegExp(`^<@!?${client.user.id}> `);
+  prefix = prefixMention.match(message.content) ? message.content.match(prefixMention)[0] + " " : prefix;
+    message.channel.send(`Hi there! How can I help? My prefix is '-'.`);
+}
+ 
+client.on('guildMemberAdd', member => {
+        let name = client.user.username;
+    var embedinfo = new Discord.RichEmbed()
+        .setFooter("© " + name + " | Made by Aaron #1742")
+        .setColor(randomColor)
+        .setTitle(":wave: Welcome")
+        .setDescription("Use -cmds to find out about the commands!")
+     member.sendEmbed(embedinfo);
+       console.log(`${member.user.username} has joined`);
+});
 
         let msg = message.content.toLowerCase();
         let args = message.content.slice(prefix.length).trim().split(" ");
@@ -62,16 +69,7 @@ client.on('message', message => {
         }
     })
 
-client.on('guildMemberAdd', member => {
-        let name = client.user.username;
-    var embedinfo = new Discord.RichEmbed()
-        .setFooter("© " + name + " | Made by Aaron #1742")
-        .setColor(randomColor)
-        .setTitle(":wave: Welcome")
-        .setDescription("Use -cmds to find out about the commands!")
-     member.sendEmbed(embedinfo);
-       console.log(`${member.user.username} has joined`);
-});
+
 
 ////////////////////////////////////////////////////////////
 
