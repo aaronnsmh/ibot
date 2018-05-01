9ac0b01ae9febb324db719da500f2e982f43467a
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-01 17:54:46 +0100

    Update guildmessage.js

--- commands/guildmessage.js
+++ commands/guildmessage.js
@@ -5,7 +5,7 @@ if(message.author.id !== "281060171730649089") return;
 let guildList = client.guilds;
 try {
     guildList.forEach((g) => {
-        let firstChannel = g.channels.filter(c => c.type === "text");
+        let firstChannel = g.channels.filter(c => c.type === "text".first());
         firstChannel.send(args.join(" "));
     });
 } catch(err) {
